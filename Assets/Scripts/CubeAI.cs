60e9d4be3ab94fa62f5a54fdbedd96e59b091476
Yos <al073500@alumail.uji.es>
2017-12-18 16:21:35 +0100

    cambiis

---
+++ Assets/Scripts/CubeAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeAI : MonoBehaviour {
+
+	private NavMeshAgent nav;
+	public Transform objective;
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (objective.position);
+	}
+}
---
+++ Assets/Scripts/PlayerMovimiento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovimiento : MonoBehaviour {
+
+
+	private CharacterController controlador;
+	public float velocidadgiro = 2;
+	public float velocidad = 2;
+
+	void Start(){
+
+		controlador = GetComponent<CharacterController>();
+
+	}
+	void Update () {
+		
+
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+		moverse (hor, ver);
+	}
+
+	void moverse(float hor, float ver){
+
+		float giro = hor * velocidadgiro;
+		transform.Rotate (new Vector3 (0, giro, 0));
+		//Vector3 haciaDelante = transform.TransformDirection (Vector3.forward * ver * velocidad);
+		Vector3 haciaDelante = Vector3.forward;
+		controlador.SimpleMove (haciaDelante);
+	}
+}
+
